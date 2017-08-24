4fc2f01c789d8b74191fd8371d1f368d80681d3c
Demetrius Berkeley <Dberkeley@fullsail.edu>
2017-08-24 16:48:16 -0400

    Completed all requirments waiting on feedback

--- js/action.js
+++ js/action.js
@@ -1,9 +1,27 @@
 const logoStart = document.getElementsByClassName('logo');
 const logo = logoStart[0];
+const sharkStart = document.getElementsByClassName('active');
 
 logo.addEventListener("click", animateLogo);
 
+let toggle = false;
+
 function animateLogo() {
-  logo.classList.add('active');
-  logo.classList.remove('logo');
+  toggle = !toggle;
+
+  if (toggle) { // Rotate
+    logo.classList.add('active');
+    logo.classList.remove('logo');
+    setTimeout(function() {
+      logo.classList.remove('active');
+      logo.classList.add('beforeActive');
+      logo.classList.add('logo');
+    }, 5000);
+
+  }
+
+  if (!toggle) { // Restart Rotate
+    logo.classList.add('logo');
+    logo.classList.remove('active');
+  }
 }
