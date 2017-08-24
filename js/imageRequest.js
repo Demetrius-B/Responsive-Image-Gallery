ef81e8773e70abb8db08afe5d5db9504661bcaf0
Demetrius Berkeley <Dberkeley@fullsail.edu>
2017-08-24 07:33:32 -0400

    Css Animation for images are live. Images are also now responsive. Working on last animation

--- js/imageRequest.js
+++ js/imageRequest.js
@@ -3,7 +3,7 @@ const body = document.querySelector('body')
 const gallery = document.querySelector('.imageGallery')
 
 // Add api link
-const apiBase = `https://api.unsplash.com/photos/?client_id=`
+const apiBase = `https://api.unsplash.com/search/photos?page=1&query=shark&client_id=`
 const userId = "822755c3909116c557faeed4824151410949735c068bc286d6643656361522fd"
 let data = []
 
@@ -18,11 +18,10 @@ console.log(url);
 fetch(url, options)
 .then(response => response.json())
 .then(responseAsJson => {
-  data = responseAsJson
+  data = responseAsJson.results
 
   for (var i = 0; i < data.length; i++) {
-    gallery.insertAdjacentHTML('afterbegin',
-    "<article class=imageDetails><img src=" + data[i].urls.small + "/> <div class=photoDetails> <div class=nameContainer><img src=img/user-icon.svg alt=user icon/> <p>" + data[i].user.name + "</p></div> <div class=likeContainer> <img src=img/heart.svg alt=heart icon/> <p>" + data[i].user.total_likes + "</p></div></div></article>"
+    gallery.insertAdjacentHTML('afterbegin', '<figure class=imageDetails><img src="' + data[i].urls.small +  '"srcset=" '+ data[i].urls.thumb + ' 200w,' +  data[i].urls.small + ' 400w, ' +  data[i].urls.regular + '  1080w" sizes="(min-width:426px) 49%, (min-width:769px) 33%, 100%""/> <figcaption> <div><img src="img/user-icon.svg" alt=user icon/> <p>' + data[i].user.name + '</p></div> <div> <img src=img/heart.svg alt=heart icon/> <p>' + data[i].user.total_likes + '</p></div></figcaption></article>'
     )
   }
 })
